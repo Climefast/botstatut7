bb050b2842f05a9c31e29fe46b4f1d6ffd42bc5b
Climefast <climefast@gmail.com>
2020-12-07 21:38:10 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,18 @@
+﻿const Discord = require('discord.js')
+
+const { prefix } = require('./config.js')
+
+const bot = new Discord.Client();
+
+bot.once('ready', () => {
+    console.log('Bot Lancer. l Create By Loki')
+    bot.user.setActivity(process.env.STATUT)
+});
+
+bot.on('message', (message) => {
+    if(message.content === '!creator') {
+     message.channel.send(process.env.CREATOR);
+	}
+});
+
+bot.login(process.env.TOKEN);
